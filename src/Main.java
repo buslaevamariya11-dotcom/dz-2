46077171400321490cf83a803aca76ad5b07143e
Mariya Buslaeva <buslaeva.mariya11@gmail.com>
2025-11-16 22:57:53 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13676;
+        int costPerMile = 20;
+
+        int bonusMiles = ticketPrice / costPerMile;
+
+        System.out.println("Стоимость билета: " + ticketPrice + " руб.");
+        System.out.println("Количество начисленных бонусных миль: " + bonusMiles);
+    }
+}
